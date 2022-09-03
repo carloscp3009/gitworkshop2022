0819de93bd97a52b75528c7dce07cfe6f5d48e9c
Carlos Cardenas <carloscp3009@hotmail.com>
2022-09-03 13:52:50 -0500

    en este commit se agrego la funcionalidad newfeature

--- myfirstscript.py
+++ myfirstscript.py
@@ -1,1 +1,3 @@
 print("my first script in git")
+
+print("Estos son cambios hechos en el segundo commit")
---
+++ newfeature.py
@@ -0,0 +1,5 @@
+print("Este es un nuevo feature")
+print("Este es un nuevo feature")
+print("Este es un nuevo feature")
+print("Este es un nuevo feature")
+print("Este es un nuevo feature")
